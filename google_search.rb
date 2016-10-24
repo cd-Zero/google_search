914c998bb2077cbcfddd93464ba6c25fb63bd75b
zero <mike.aduayi@gmail.com>
2016-10-24 12:17:06 -0230

    loop fixed and operational, also loop searches through google, yahoo and bing

--- google_search.rb
+++ google_search.rb
@@ -4,8 +4,8 @@ require 'rest-client'
 puts "What would you like to search for?"
 
 #stores input of search query into variable search
-search = gets.chomp
-
+search 			=  	gets.chomp
+#search turned into string and put in url to complete search
 search_google 	=	"https://www.google.com/search?q=#{search}"
 
 search_yahoo 	= 	"https://search.yahoo.com/search;_ylt=A0LEVxt7AAhYVfYATJOl87UF;_ylc=X1MDOTU4MTA0NjkEX3IDMgRmcgN5bwRncHJpZANEbGhyNzlNalJVYVNnT1FKWG14ZmZBBG5fcnNsdAMwBG5fc3VnZwMxMARvcmlnaW4Dc2VhcmNoLnlhaG9vLmNvbQRwb3MDMARwcXN0cgMEcHFzdHJsAwRxc3RybAMyBHF1ZXJ5A3lvBHRfc3RtcAMxNDc2OTE5NDI5?p=#{search}"
@@ -13,25 +13,25 @@ search_yahoo 	= 	"https://search.yahoo.com/search;_ylt=A0LEVxt7AAhYVfYATJOl87UF;
 search_bing 	= 	"https://www.bing.com/search?q=#{search}"
 
 
-#stores input of url -note variable search is used at end of url- into variable response
-response =  [RestClient.get search_google,RestClient.get search_yahoo,RestClient.get search_bing]
- 
-response do |respond| 
+#!!! clearify what RestClient.get does !!!!
+#stores value ofRestClient.get into search engines names 
+google 	= 	RestClient.get search_google
+yahoo 	= 	RestClient.get search_yahoo
+bing 	= 	RestClient.get search_bing
+
+#creates array for RestClient to go through
+response =  [google,yahoo,bing]
+
+# creates loop using each method and outputs response attached to code, headers,cookies methods
+response.each do |respond| 
 
 	puts""
 	puts "response code :"
 	puts respond.code
-	
 	puts ""
 	puts "response header :"
-	puts response.header
-	
+	puts respond.headers
 	puts ""
 	puts "response cookie :"
-	puts response.cookie
-	
-	puts ""
-	puts "response history:"
-	puts response.history
-
+	puts respond.cookies
 end
